a8e3f1f18d8cab4b110160a2f320a15bd84fadf6
pengshixing <2644378911@qq.com>
2023-07-02 20:17:26 +0800

    添加开屏动画，后续可替换为引导页或开屏动画

---
+++ logindemo/src/main/java/com/example/logindemo/activity/GuideActivity.java
@@ -0,0 +1,20 @@
+package com.example.logindemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.logindemo.R;
+
+
+/**
+ * app 引导页
+ */
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
